3ef279e962ba07bb720bc9023eebdb4dbe88cba5
Maxwell Monteiro <maxmonte.vix@gmail.com>
2020-03-28 22:30:45 -0300

    Version FireStore + Paciente Views

--- app/src/main/java/com/mx/mobi_flm/MainActivity.java
+++ app/src/main/java/com/mx/mobi_flm/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.Query;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.DocumentReference;
@@ -25,6 +26,7 @@ import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -36,30 +38,34 @@ private Button btn1, btn2;
     //aux members
     private String[] pacientes=new String[10];
     Integer i=0;
+    Integer j=0;
 
 // Firebase members
+    private FirebaseAuth mAuth;
     private FirebaseFirestore db;
     private CollectionReference myRef ;
+    private DocumentReference myDoc;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //CustomListAdapter myAdapter = new CustomListAdapter(this, R.layout.activity_main,this,imageArray,titleArray,infoArray);
-        //ArrayAdapter myAdapter = new ArrayAdapter<String>(this,R.layout.activity_main,titleArray);
+
         //Views connection
         btn1 = findViewById(R.id.button1);
        textView2= findViewById(R.id.textview2);
 
        //Firebase
-        //db = FirebaseFirestore.getInstance();
+        db = FirebaseFirestore.getInstance();
         myRef=db.collection("Users");
+        myDoc=myRef.document("Latussa");
+
         myRef.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()) {
-                    for (QueryDocumentSnapshot document : task.getResult()) {
+                    for (QueryDocumentSnapshot document : Objects.requireNonNull(task.getResult())) {
                         Log.i("FIREBASE", i.toString()+ ":"+ document.getId() + " => " +document.getData());
                         pacientes[i++]= document.getId();
                     }
@@ -71,7 +77,13 @@ private Button btn1, btn2;
 
       btn1.setOnClickListener(new View.OnClickListener() {
           public void onClick(View v) {
-              textView2.setText(pacientes[i]);
+              if(j<i){
+                  textView2.setText(pacientes[j]);
+                  j++;
+              }
+              else{
+                  textView2.setText(pacientes[0]);
+              }
           }
 
 
