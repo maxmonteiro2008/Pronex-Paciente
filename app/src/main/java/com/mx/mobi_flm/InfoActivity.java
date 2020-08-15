1eed107a6ce4bc59c23ac5e654182cc779163fde
Maxwell Monteiro <maxmonte.vix@gmail.com>
2020-08-15 10:21:53 -0300

    Ver 1.0: Auth + Opcoes-Final2 + Hora Medicamento + Camera2 + Medicacao + Sentimento+ info

--- app/src/main/java/com/mx/mobi_flm/InfoActivity.java
+++ app/src/main/java/com/mx/mobi_flm/InfoActivity.java
@@ -3,10 +3,7 @@ package com.mx.mobi_flm;
 import android.os.Bundle;
 import android.widget.TextView;
 
-import com.google.android.material.appbar.CollapsingToolbarLayout;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
 public class InfoActivity extends AppCompatActivity {
 
@@ -15,19 +12,10 @@ public class InfoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_info);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
-        toolBarLayout.setTitle(getTitle());
+        setContentView(R.layout.content_scrolling);
+
 
         textView = (TextView) findViewById(R.id.text1);
-        //fab.setOnClickListener(new View.OnClickListener() {
-        //    @Override
-        //    public void onClick(View view) {
-        //       Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-        //                .setAction("Action", null).show();
-        //    }
-        //});
+
     }//onCReate
 }//Activity
